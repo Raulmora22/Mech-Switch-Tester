636339b37ae20ff956a20163a5a22e7f35231082
Raulmora22 <raulprogrammerr@gmail.com>
2024-05-16 12:58:22 +0200

    Base

---
+++ src/app/components/Translation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Translation() {
+  return (
+    <div className="pt-8 ">
+      <div className="bg-black ">hola</div>
+    </div>
+  );
+}
+
+export default Translation;
